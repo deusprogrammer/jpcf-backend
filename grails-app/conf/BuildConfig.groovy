aaf2e1964189cf481018a29c73e70c7a685b1625
Michael Main <michael.main@utsa.edu>
2013-01-12 06:44:02 -0800

    Oops...forgot to add cors plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -41,7 +41,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.0"
         runtime ":resources:1.1.6"
-        //compile ":file-uploader:1.2.1"
+		runtime ":cors:1.0.3"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
